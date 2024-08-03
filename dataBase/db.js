681b4d4a0d9fe2c6f286860d182a334f9a005e4f
Hissen Tutu <hissentutu@gmail.com>
2024-08-03 12:44:55 +0300

    modified the database connectivity

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,7 +1,9 @@
   const mongoose=require('mongoose')
+  require('dotenv').config()
+  const Mongo_URI=process.env.Mongo_URI;
 
 
 
   mongoose.
-  connect('mongodb+srv://test123:test123@cluster0.awvv3s7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  connect(Mongo_URI)
   .then(()=>console.log('db connected successfully!')).catch((e)=>console.log(e))
